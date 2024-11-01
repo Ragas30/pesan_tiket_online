511dccbcd663a1fe51827a46eb4e25763d9fdc43
Roman Keanu Mufid <romankeanumufid@gmail.com>
2024-11-01 11:33:53 +0700

    Configurating connection to database

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import sequelize from './config/db.js';
 
 dotenv.config();
 const app = express();
+const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/', (req, res) => {
   res.status(200).send('<h1>Hello World</h1>')
-})
+});
 
-const PORT = process.env.SERVER_PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  })
+}).catch(err => {
+  console.log("Unable to connect to database " + err);
 });
